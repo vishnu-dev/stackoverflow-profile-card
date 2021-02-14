5d78e997268e3d90e925bf107c9aa085abd6fcc1
Vishnudev K <vishnu@sumyag.com>
2021-02-15 02:33:52 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from "react";
+import ProfileCard from './components/ProfileCard/ProfileCard';
+import Configurator from './components/Configurator/Configurator';
 
 function App() {
+  const [config, setConfig] = useState({
+      size: 'large',
+      gradient: ['#20bf55', '#01baef'],
+      textColor: '#FFFFFF'
+  });
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <link rel="preconnect" href="https://fonts.gstatic.com"></link>
+      <link href="https://fonts.googleapis.com/css2?family=Pacifico&display=swap" rel="stylesheet"></link>
+      <link href="https://fonts.googleapis.com/css2?family=Saira+Condensed:wght@200;400;600&display=swap" rel="stylesheet"></link>
+      <link href="https://fonts.googleapis.com/css2?family=Alegreya+Sans:wght@800&display=swap" rel="stylesheet"></link>
+      <div className="Main">
+        <div className="LeftMain" style={{backgroundImage: 'url(/lightbg.png)', color: '#000'}}>
+          <div style={{backgroundColor: 'rgba(255, 255, 255, 0.6)', borderRadius: 13 + 'px', padding: '10px'}}>
+          <img src="https://stackoverflow.design/assets/img/logos/so/logo-stackoverflow.png" height="40"></img>
+          </div>
+          <p className="Title">Profile Card</p>
+          <ProfileCard config={config}></ProfileCard>
+        </div>
+        <div className="RightMain">
+          <Configurator onChange={setConfig}></Configurator>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/components/Configurator/Configurator.js
@@ -0,0 +1,157 @@
+import React from "react";
+import "./Configurator.css";
+import Paper from "@material-ui/core/Paper";
+import FullscreenIcon from "@material-ui/icons/Fullscreen";
+import FullscreenExitIcon from "@material-ui/icons/FullscreenExit";
+import Button from "@material-ui/core/Button";
+import { makeStyles } from "@material-ui/core";
+import Grid from "@material-ui/core/Grid";
+import { AutoSizer, ColumnSizer, Grid as VirtualGrid } from "react-virtualized";
+import "react-virtualized/styles.css";
+import gradientsJSON from "../../data/gradients.json";
+
+const useStyles = makeStyles((theme) => ({
+    grid: {
+        margin: theme.spacing(0),
+        flexGrow: 0,
+        maxWidth: `100%`,
+        flexBasis: `100%`,
+    },
+}));
+
+const hexToR = (h) => parseInt(cutHex(h).substring(0, 2), 16);
+const hexToG = (h) => parseInt(cutHex(h).substring(2, 4), 16);
+const hexToB = (h) => parseInt(cutHex(h).substring(4, 6), 16);
+const cutHex = (h) => (h.charAt(0) === "#" ? h.substring(1, 7) : h);
+
+const getBorW = (hex) => {
+    const threshold = 130; /* about half of 256. Lower threshold equals more dark text on dark background  */
+
+    const hRed = hexToR(hex);
+    const hGreen = hexToG(hex);
+    const hBlue = hexToB(hex);
+
+    const cBrightness = (hRed * 299 + hGreen * 587 + hBlue * 114) / 1000;
+    if (cBrightness > threshold) {
+        return "#000000";
+    } else {
+        return "#ffffff";
+    }
+};
+
+const Configurator = ({onChange}) => {
+    const classes = useStyles();
+    const [size, setSize] = React.useState("small");
+    const [gradients, setGradients] = React.useState([]);
+    const [config, setConfig] = React.useState({
+        size: 'large',
+        gradient: ['#20bf55', '#01baef'],
+        textColor: '#FFFFFF'
+    });
+
+    const columnCount = 3;
+
+    React.useEffect(() => {
+        setGradients(gradientsJSON);
+    }, []);
+
+    const renderCell = ({ columnIndex, key, rowIndex, style }) => {
+        const index = rowIndex * columnCount + columnIndex;
+        const datum = gradients[index];
+        const cellStyle = {
+            backgroundImage: `linear-gradient(to right, ${datum.colors.join(
+                ", "
+            )})`,
+            color: getBorW(datum.colors[0]),
+        };
+        return (
+            <div
+                style={{ ...cellStyle, ...style }}
+                key={key}
+                className="Gradient"
+                onClick={() => updateConfig({gradient: datum.colors, textColor: cellStyle.color})}
+            >
+                {gradients[index].name.toUpperCase()}
+            </div>
+        );
+    };
+
+    const updateConfig = (newConfig) => {
+        setConfig({...config, ...newConfig});
+        onChange({...config, ...newConfig});
+    };
+
+    return (
+        <React.Fragment>
+            <Grid container spacing={3} className={classes.grid}>
+                <Grid item xs={12}>
+                    <Paper className="Section">
+                        <p className="Header">Card Size</p>
+                        <Button
+                            className="ButtonGradient"
+                            variant={
+                                config.size === "large" ? "contained" : "outlined"
+                            }
+                            value="large"
+                            color="primary"
+                            onClick={() => updateConfig({size: "large"})}
+                        >
+                            <FullscreenIcon /> Large
+                        </Button>
+                        <Button
+                            className="ButtonGradient"
+                            variant={
+                                config.size === "small" ? "contained" : "outlined"
+                            }
+                            value="small"
+                            color="primary"
+                            onClick={() => updateConfig({size: "small"})}
+                        >
+                            <FullscreenExitIcon /> Small
+                        </Button>
+                    </Paper>
+                </Grid>
+                <Grid item xs={12}>
+                    <Paper className="Section">
+                        <p className="Header">Background Gradient</p>
+                        <AutoSizer disableHeight>
+                            {({ width }) => (
+                                <ColumnSizer
+                                    columnMinWidth={50}
+                                    columnCount={columnCount}
+                                    width={width}
+                                >
+                                    {({
+                                        adjustedWidth,
+                                        getColumnWidth,
+                                        registerChild,
+                                    }) => (
+                                        <VirtualGrid
+                                            ref={registerChild}
+                                            cellRenderer={renderCell}
+                                            columnCount={columnCount}
+                                            columnWidth={getColumnWidth}
+                                            height={300}
+                                            rowCount={Math.floor(
+                                                gradients.length / columnCount
+                                            )}
+                                            rowHeight={50}
+                                            width={adjustedWidth}
+                                            style={{outline: 'none'}}
+                                        />
+                                    )}
+                                </ColumnSizer>
+                            )}
+                        </AutoSizer>
+                    </Paper>
+                </Grid>
+            </Grid>
+        </React.Fragment>
+    );
+};
+
+Configurator.propTypes = {};
+
+Configurator.defaultProps = {};
+
+export default Configurator;
